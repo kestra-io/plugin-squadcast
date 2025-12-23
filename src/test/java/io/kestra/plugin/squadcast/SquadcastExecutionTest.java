eee4eddf8fe0991d60109ea32d39d8b76d0168d9
Malay Dewangan <malaydewangan310@gmail.com>
2025-12-23 17:38:27 +0530

    feat(): introduce Squadcast plugin

---
+++ src/test/java/io/kestra/plugin/squadcast/SquadcastExecutionTest.java
@@ -0,0 +1,46 @@
+package io.kestra.plugin.squadcast;
+
+import io.kestra.core.junit.annotations.KestraTest;
+import io.kestra.core.repositories.LocalFlowRepositoryLoader;
+import io.kestra.core.runners.TestRunner;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Objects;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+@KestraTest
+class SquadcastExecutionTest extends AbstractSquadcaseTest {
+    @Inject
+    protected TestRunner runner;
+
+    @Inject
+    protected LocalFlowRepositoryLoader repositoryLoader;
+
+    @BeforeEach
+    protected void init() throws IOException, URISyntaxException {
+        repositoryLoader.load(Objects.requireNonNull(SquadcastExecutionTest.class.getClassLoader().getResource("flows")));
+        this.runner.run();
+    }
+
+    @Test
+    void flow() throws Exception {
+        var failedExecution = runAndCaptureExecution(
+            "main-flow-that-fails",
+            "squadcast"
+        );
+
+        String receivedData = waitForWebhookData(() -> FakeWebhookController.data,5000);
+
+        assertThat(receivedData, containsString(failedExecution.getId()));
+        assertThat(receivedData, containsString("https://mysuperhost.com/kestra/ui"));
+        assertThat(receivedData, containsString("[io.kestra.tests] main-flow-that-fails"));
+        assertThat(receivedData, containsString("Failed on task `failed`"));
+        assertThat(receivedData, containsString("Kestra Squadcast alert"));
+    }
+}
